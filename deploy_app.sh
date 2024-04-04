cccf3ee03eada325d975eb0453cb5b96f6bff392
SpanDuy <hdhrbux@gmail.com>
2024-04-04 22:16:20 +0300

    feat: add auth

---
+++ deploy_app.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Очистка проекта Maven и установка зависимостей
+mvn clean install dependency:copy-dependencies
+
+# Создание пользовательского образа JRE с jlink
+jlink --add-modules java.base,java.desktop,java.instrument,java.management,java.net.http,java.prefs,java.rmi,java.scripting,java.security.jgss,java.security.sasl,java.sql.rowset,jdk.compiler,jdk.jfr,jdk.unsupported --strip-debug --no-man-pages --no-header-files --compress=2 --output javaruntime
+
+# Создание директории для сборки приложения и переход в неё
+mkdir -p build-app && cd build-app
+
+# Извлечение слоев приложения из JAR-файла
+java -Djarmode=layertools -jar ../target/TestHub-0.0.1-SNAPSHOT.jar extract
+
+# Запуск Docker Compose для поднятия контейнеров
+docker-compose up -d
---
+++ src/main/resources/db/migration/V002__Add_Test_data.sql
@@ -0,0 +1,3 @@
+-- Вставка тестовых данных в таблицу Users
+INSERT INTO users (username, email, password, role)
+VALUES ('User1', 'user1@example.com', '$2a$10$NDX3/9zF/iTd6X7ifXUDjeyw7h4mUX1cxoQMX1DVwfl7gXCeihMAS', 'ROLE_USER');
